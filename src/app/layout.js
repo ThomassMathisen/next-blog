c76252702fceca2f264dd8ba2d15732998806cad
ThomassMathisen <thomasmathisen@hotmail.com>
2024-01-23 19:06:08 +0100

    Creates footer and components folders

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import Navbar from "@/components/navbar/Navbar";
 import "./globals.css";
 import { Inter } from "next/font/google";
+import Footer from "@/components/footer/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,6 +18,7 @@ export default function RootLayout({ children }) {
           <div className="wrapper">
             <Navbar />
             {children}
+            <Footer />
           </div>
         </div>
       </body>
