4033b597bd347fcf5a4574abb9436810bd40c23a
ThomassMathisen <thomasmathisen@hotmail.com>
2024-01-23 18:55:05 +0100

    Creates navbar

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+import Navbar from "@/components/navbar/Navbar";
 import "./globals.css";
 import { Inter } from "next/font/google";
 
@@ -13,7 +14,10 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
         <div className="container">
-          <div className="wrapper">{children}</div>
+          <div className="wrapper">
+            <Navbar />
+            {children}
+          </div>
         </div>
       </body>
     </html>
