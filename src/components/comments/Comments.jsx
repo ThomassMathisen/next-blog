f4be3a00d9bfb9b095378dcb566046231c65b250
ThomassMathisen <thomasmathisen@hotmail.com>
2024-01-24 18:50:27 +0100

    Creates login page design

--- src/components/comments/Comments.jsx
+++ src/components/comments/Comments.jsx
@@ -3,14 +3,19 @@ import styles from "./comments.module.css";
 import Image from "next/image";
 
 const Comments = () => {
+  const status = "authenticated";
+
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Comments</h1>
-      <div className={styles.write}>
-        <textarea className={styles.input} placeholder="Write a comment.." />
-        <button className={styles.button}>Send</button>
-      </div>
-      <Link href="/login">Login to comment</Link>
+      {status === "authenticated" ? (
+        <div className={styles.write}>
+          <textarea className={styles.input} placeholder="Write a comment.." />
+          <button className={styles.button}>Send</button>
+        </div>
+      ) : (
+        <Link href="/login">Login to comment</Link>
+      )}
       <div className={styles.comments}>
         <div className={styles.comment}>
           <div className={styles.user}>
