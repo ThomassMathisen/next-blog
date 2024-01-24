7fd8e299b6bcddd86569de1c879bd104b8c16cbf
ThomassMathisen <thomasmathisen@hotmail.com>
2024-01-24 17:58:57 +0100

    Creates blog page

--- src/components/pagination/Pagination.jsx
+++ src/components/pagination/Pagination.jsx
@@ -1,7 +1,31 @@
-import React from "react";
+"use client";
 
-const Pagination = () => {
-  return <div>Pagination</div>;
+import styles from "./pagination.module.css";
+import { useRouter } from "next/navigation";
+
+const Pagination = ({ page, hasPrev, hasNext }) => {
+  const router = useRouter();
+
+  return (
+    <div className={styles.container}>
+      <button
+        className={styles.button}
+        disabled
+        {...hasPrev}
+        onClick={() => router.push(`?page=${page - 1}`)}
+      >
+        Previous
+      </button>
+      <button
+        className={styles.button}
+        disabled
+        {...hasNext}
+        onClick={() => router.push(`?page=${page + 1}`)}
+      >
+        Next
+      </button>
+    </div>
+  );
 };
 
 export default Pagination;
