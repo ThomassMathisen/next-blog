306d18fc529a8125dc2c23609ea1f8cf452278cc
ThomassMathisen <thomasmathisen@hotmail.com>
2024-01-27 17:23:27 +0100

    Setting up api for posts, comments and categories

--- src/app/posts/[slug]/page.jsx
+++ src/app/posts/[slug]/page.jsx
@@ -3,36 +3,61 @@ import styles from "./singlePost.module.css";
 import Menu from "@/components/menu/Menu";
 import Comments from "@/components/comments/Comments";
 
-const SinglePage = () => {
+const getData = async (slug) => {
+  const res = await fetch(`http://localhost:3000/api/posts/${slug}`, {
+    cache: "no-store",
+  });
+
+  if (!res.ok) {
+    throw new Error("Failed");
+  }
+
+  return res.json();
+};
+
+const SinglePage = async ({ params }) => {
+  const { slug } = params;
+
+  const data = await getData(slug);
+
   return (
     <div className={styles.container}>
       <div className={styles.infoContainer}>
         <div className={styles.textContainer}>
-          <h1 className={styles.title}>Blog Post</h1>
+          <h1 className={styles.title}>{data.title}</h1>
           <div className={styles.user}>
-            <div className={styles.userImageContainer}>
-              <Image src="/p1.jpeg" alt="" fill className={styles.avatar} />
-            </div>
+            {data?.user?.image && (
+              <div className={styles.userImageContainer}>
+                <Image
+                  src={data.user.image}
+                  alt=""
+                  fill
+                  className={styles.avatar}
+                />
+              </div>
+            )}
             <div className={styles.userTextContainer}>
-              <span className={styles.username}>James Franco</span>
-              <span className={styles.date}>01.01.24</span>
+              <span className={styles.username}>{data?.user.name}</span>
+              <span className={styles.date}>
+                {data.createdAt.substring(0, 10)}
+              </span>
             </div>
           </div>
         </div>
-        <div className={styles.imageContainer}>
-          <Image src="/p1.jpeg" alt="" fill className={styles.image} />
-        </div>
+        {data?.img && (
+          <div className={styles.imageContainer}>
+            <Image src={data.img} alt="" fill className={styles.image} />
+          </div>
+        )}
       </div>
       <div className={styles.content}>
         <div className={styles.post}>
-          <div className={styles.description}>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolor
-            animi nulla totam voluptas, vero debitis in tempore nihil assumenda
-            alias, nesciunt deserunt sequi eaque ipsa, explicabo doloremque nam
-            veniam. Recusandae?
-          </div>
+          <div
+            className={styles.description}
+            dangerouslySetInnerHTML={{ __html: data?.desc }}
+          />
           <div className={styles.comment}>
-            <Comments />
+            <Comments postSlug={slug} />
           </div>
         </div>
         <Menu />
--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -2,26 +2,29 @@ import Image from "next/image";
 import styles from "./card.module.css";
 import Link from "next/link";
 
-const Card = () => {
+const Card = ({ key, item }) => {
   return (
-    <div className={styles.container}>
-      <div className={styles.imageContainer}>
-        <Image src="/p1.jpeg" alt="" fill className={styles.image} />
-      </div>
+    <div className={styles.container} key={key}>
+      {item.img && (
+        <div className={styles.imageContainer}>
+          <Image src={item.img} alt="" fill className={styles.image} />
+        </div>
+      )}
       <div className={styles.textContainer}>
         <div className={styles.detail}>
-          <span className={styles.date}>01.01.2024 - </span>
-          <span className={styles.category}>Fashion</span>
+          <span className={styles.date}>
+            {item.createdAt.substring(0, 10)} -{" "}
+          </span>
+          <span className={styles.category}>{item.catSlug}</span>
         </div>
-        <Link href="/blog?=fashion">
-          <h1>Title</h1>
+        <Link href={`/posts/${item.slug}`}>
+          <h1>{item.title}</h1>
         </Link>
-        <div className={styles.desc}>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod,
-          mollitia molestias assumenda magnam ea repudiandae quis officia veniam
-          eum sit iste obcaecati sapiente esse beatae optio ex animi vel iure.
-        </div>
-        <Link href="/posts?=fashion">Read More</Link>
+        <div
+          className={styles.desc}
+          dangerouslySetInnerHTML={{ __html: item?.desc.substring(0, 60) }}
+        />
+        <Link href={`/posts/${item.slug}`}>Read More</Link>
       </div>
     </div>
   );
